8fda54bb6d9d1401501abb7ad940b0a8cd76978f
Yar83Men <71335336+Yar83Men@users.noreply.github.com>
2021-09-01 19:11:38 +0300

    Update Binary_search.java

--- Binary_search.java
+++ Binary_search.java
@@ -10,25 +10,22 @@ public class Binary_search {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 1000);
         }
+
         //System.out.println(Arrays.toString(arr));
-        int number = 325;
+        int number = 125;
 
         Binary_search obj = new Binary_search();
-        System.out.println("Position of element : " + obj.bin_search(number, arr) + "  " + obj.count());
+        System.out.println("Position of element : " + obj.bin_search(number, arr) + " on position: " + obj.count);
     }
 
-    public String count() {
-        count++;
-        return "Count of operations: " + count;
-    }
-
-    public int bin_search(int num, int[] arr) {
+    int bin_search(int num, int[] arr) {
         Arrays.sort(arr);
         int start = 0;
         int end = arr.length - 1;
         while (start <= end) {
             int mid = (end + start) / 2;
-            count();
+            //count();
+            count++;
             int guess = arr[mid];
             if (guess == num) return mid;
             if (num < guess) end = mid - 1;
